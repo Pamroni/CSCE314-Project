2cb188d9238f3ee5e3f240f9cc3a91574c00bfbb
Alex Ramponi <aramponi@tamu.edu>
2019-11-24 02:29:24 -0600

    Starting the code for part 2
    Starting part 2

--- src/Project1.java
+++ src/Project1.java
@@ -1,29 +1,9 @@
-/*
-* 	Alex Ramponi
-* 	No changes made in this file
-* 	Lupoli CSCE-314-502, Fall 2019
-*/
 public class Project1 {
 	public static void main(String[] args) 
 	{
-		// Instantiate PrimeOperations Class
-		PrimeOperations testOne = new PrimeOperations();
-
-		// Generate Primes and test.
-		testOne.generatePrimes(21);
-		testOne.printPrimes();
-
-		// Generate and test Twin Primes
-		PrimeOperations testTwo = new PrimeOperations();
-		testTwo.generatePrimes(100);
-		testTwo.generateTwinPrimes();
-		testTwo.printTwins();
-
-		// Generate and test Hexagonal crosses
-		PrimeOperations testThree = new PrimeOperations();
-		testThree.generatePrimes(2000);
-		testThree.generateTwinPrimes();
-		testThree.generateHexPrimes();
-		testThree.printHexes();
+		// Instantiate Primes Class
+		Primes p = new Primes();
+		
+		MainWindow mw = new MainWindow(Config.APPLICATIONNAME, p);
 	}
 }
