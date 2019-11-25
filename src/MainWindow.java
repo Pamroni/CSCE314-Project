3781cf909bc2640e9c62cf9dcdb8630d15a73046
Alex Ramponi <aramponi@tamu.edu>
2019-11-25 16:38:22 -0600

    Removed Eclipse UI class and created the mainwindow constructor

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,34 +20,120 @@ public class MainWindow extends JFrame
 	
 	MainWindow(String name, Primes p)
 	{
-		lblStatus = new JLabel();
-
 		m_Primes = p;
+		setTitle(name);
 
-		JFrame mainFrame = new JFrame(name);
-		mainFrame.setSize(1000,400);
-		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//
-//		Container mainContentPane = mainFrame.getContentPane();
-//		mainContentPane.setBackground(Color.decode("#500000"));
-//
-//
-//		JPanel primesPanel = new JPanel();
-//		primesPanel.setLayout(new FlowLayout());
-//
-//		tfPrimeFileName = new JTextField();
-//		tfPrimeFileName.setSize(886,58);
-//		//primesPanel.setLayout(null);
-//		lblPrimeCount = new JLabel();
-//		lblPrimeCount.setSize(114,66);
-//		primesPanel.add(lblPrimeCount);
-//		primesPanel.add(tfPrimeFileName);
-//
-//		mainContentPane.add(primesPanel);
-//
-//		mainFrame.pack();
-//		mainFrame.setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 1000, 400);
+		JPanel contentPane = new JPanel();
+		contentPane.setBackground(new Color(50, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
+
+		//==================Primes Panel==================
+		JPanel primesPanel = new JPanel();
+		primesPanel.setBounds(1, 1, 999, 122);
+		contentPane.add(primesPanel);
+		primesPanel.setLayout(null);
+
+		tfPrimeFileName = new JTextField();
+		tfPrimeFileName.setText("primes.txt");
+		tfPrimeFileName.setBounds(new Rectangle(6, 5, 886, 58));
+		primesPanel.add(tfPrimeFileName);
+		tfPrimeFileName.setColumns(10);
+
+		lblCrossCount = new JLabel("1000");
+		lblCrossCount.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblCrossCount.setBounds(900, 10, 93, 58);
+		primesPanel.add(lblCrossCount);
+
+		JLabel primesFileLabel = new JLabel("Primes File");
+		primesFileLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		primesFileLabel.setBounds(6, 66, 790, 66);
+		primesPanel.add(primesFileLabel);
+
+		JButton btnLoad = new JButton("Load");
+		btnLoad.setBounds(800, 66, 85, 55);
+		primesPanel.add(btnLoad);
+
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(890, 66, 85, 55);
+		primesPanel.add(btnSave);
+		//=======================================================
+
+		//==================Hexagon Cross Panel==================
+		JPanel crossPanel = new JPanel();
+		crossPanel.setBounds(1, 126, 999, 122);
+		contentPane.add(crossPanel);
+		crossPanel.setLayout(null);
+
+		tfCrossFileName = new JTextField();
+		tfCrossFileName.setText("crosses.txt");
+		tfCrossFileName.setBounds(new Rectangle(6, 5, 886, 58));
+		crossPanel.add(tfCrossFileName);
+		tfCrossFileName.setColumns(10);
+
+		lblPrimeCount = new JLabel("10000");
+		lblPrimeCount.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		lblPrimeCount.setBounds(900, 10, 93, 58);
+		crossPanel.add(lblPrimeCount);
+
+		JLabel crossFileLabel = new JLabel("Hexagon Cross File");
+		crossFileLabel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+		crossFileLabel.setBounds(6, 66, 790, 66);
+		crossPanel.add(crossFileLabel);
+
+		JButton btnCrossLoad = new JButton("Load");
+		btnCrossLoad.setBounds(800, 66, 85, 55);
+		crossPanel.add(btnCrossLoad);
+
+		JButton btnCrossSave = new JButton("Save");
+		btnCrossSave.setBounds(890, 66, 85, 55);
+		crossPanel.add(btnCrossSave);
+		////=======================================================
+
+
+		//==================Generate button Panel==================
+		JPanel generatePanel = new JPanel();
+		generatePanel.setBounds(1, 252, 999, 80);
+		contentPane.add(generatePanel);
+		generatePanel.setLayout(null);
+
+		JButton btnGeneratePrimes = new JButton("Generate Primes");
+		btnGeneratePrimes.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnGeneratePrimes.setBounds(30, 10, 300, 65);
+		generatePanel.add(btnGeneratePrimes);
+
+		JButton btnGenerateCrosses = new JButton("Generate Crosses");
+		btnGenerateCrosses.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnGenerateCrosses.setBounds(660, 10, 300, 65);
+		generatePanel.add(btnGenerateCrosses);
+
+		lblLargestPrime = new JLabel("The largest prime has 0 digits.");
+		lblLargestPrime.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblLargestPrime.setBounds(400, 10, 300, 30);
+		generatePanel.add(lblLargestPrime);
+
+		lblLargestCross = new JLabel("The largest cross has 0 and 0 digits.");
+		lblLargestCross.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblLargestCross.setBounds(385, 43, 300, 30);
+		generatePanel.add(lblLargestCross);
+		//=======================================================
+
+		//==================Status Panel==================
+		JPanel statusPanel = new JPanel();
+		statusPanel.setBounds(1, 335, 999, 29);
+		contentPane.add(statusPanel);
+		statusPanel.setLayout(null);
+
+		lblStatus = new JLabel("Status: Bored.");
+		lblStatus.setBounds(6, 6, 900, 16);
+		statusPanel.add(lblStatus);
+		//=======================================================
+
+		setVisible(true);
 	}
 
 	protected void popupGeneratePrimes()
